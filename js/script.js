f6d56ec0b4917b7cb1b589321b557bccef7304e2
gustavon <gustavon>
2026-03-02 12:26:55 -0300

    arrumando validação

--- js/script.js
+++ js/script.js
@@ -1,37 +1,42 @@
 const tarefas = [];
 
-
 const form = document.querySelector("#formTarefa");
 const input = document.querySelector("#inputTarefa");
 const lista = document.querySelector("#listaTarefas");
 const mensagemErro = document.querySelector("#mensagemErro");
 
+function validarTarefa(texto) {
+    if (texto.trim() === "") {
+        mensagemErro.textContent = "Digite uma tarefa válida.";
+        return false;
+    }
+
+    mensagemErro.textContent = "";
+    return true;
+}
+
 function renderTarefas() {
     lista.textContent = "";
-    
-    tarefas.forEach(function(tarefas) {
+
+    tarefas.forEach(function(tarefa) {
         const li = document.createElement("li");
-        li.textContent = tarefas;
+        li.textContent = tarefa;
         lista.appendChild(li);
     });
 }
 
-
-
-form.addEventListener("submit", function(event){
+form.addEventListener("submit", function(event) {
     event.preventDefault();
 
     const texto = input.value;
 
-    // if (!validarTarefa(texto)) {
-    //     return;
+    if (!validarTarefa(texto)) {
+        return;
+    }
 
-    // }
-    
-    tarefas.push(texto);
+    tarefas.push(texto.trim());
 
     renderTarefas();
 
     input.value = "";
 });
-
